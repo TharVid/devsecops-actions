d0f4163cf5f27e088d6e31d3a66cbec8ec078ef3
stharvid <50536429+TharVid@users.noreply.github.com>
2024-05-06 07:59:54 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+# example_code.py
+
+# Hardcoded AWS Access Key and Secret Key (just for example purposes)
+aws_access_key_id = "AKIAEXAMPLE12345678"
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# Hardcoded API Key (example)
+api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+
+# Hardcoded Password
+db_password = "P@ssw0rd1234!"
